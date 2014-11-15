463a3669640404c9a131363ba60ff11a1756adb0
-mxb- <mxb.atria@gmail.com>
2014-11-15 12:18:57 -0800

    setup Controllers

---
+++ Google API Boss/AllAPIViewController.h
@@ -0,0 +1,13 @@
+//
+//  AllAPIViewController.h
+//  Google API Boss
+//
+//  Created by Benjamin Friedman on 11/15/14.
+//  Copyright (c) 2014 Uphouseworks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AllAPIViewController : UIViewController
+
+@end
---
+++ Google API Boss/Google API Boss/APIViewController.m
@@ -0,0 +1,37 @@
+//
+//  APIViewController.m
+//  Google API Boss
+//
+//  Created by Benjamin Friedman on 11/15/14.
+//  Copyright (c) 2014 Uphouseworks. All rights reserved.
+//
+
+#import "APIViewController.h"
+
+@interface APIViewController ()
+
+@end
+
+@implementation APIViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
