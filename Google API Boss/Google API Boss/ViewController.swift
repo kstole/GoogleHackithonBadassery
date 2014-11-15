d15b7de62ea9d134815082046ca9f96182156b76
Kyler Stole <stolek@onid.oregonstate.edu>
2014-11-15 12:54:53 -0800

    Added swift View Controller

---
+++ Google API Boss/Google API Boss/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Google API Boss
+//
+//  Created by Kyler Stole on 11/15/14.
+//  Copyright (c) 2014 Uphouseworks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+	
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+	
+	@IBAction func randomAPI() {
+		
+	}
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
